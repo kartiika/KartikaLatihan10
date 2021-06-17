3e71c2bc6c2df9318c7dda0d0539a1afeb143a72
kartiika <kartikaidm5@gmail.com>
2021-06-17 21:17:29 +0700

    latihan

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <Title title="Catatan pengeluaran"/>
+  <FormMahasiswa @entri-mahasiswa = "entriMahasiswa($event)"/>
+  <ListMahasiswa v-if="dataMahasiswa.length>0" :dataMahasiswalist="dataMahasiswa"/>
+</template>
+
+<script>
+import Title from './components/Title.vue'
+import FormMahasiswa from './components/FormMahasiswa.vue'
+import ListMahasiswa from './components/ListMahasiswa.vue'
+export default {
+  name: 'App',
+  components: {
+    Title,
+    ListMahasiswa,
+    FormMahasiswa,
+  },
+  data(){
+    return{
+      dataMahasiswa : [
+        {nim : 'D1118111053', nama : 'Kartika', kelas : 'IF 18 B'},
+        {nim : 'D111811123', nama : 'salsa', kelas : 'IF 18 B'},
+        {nim : 'D111811131', nama : 'Indah', kelas : 'IF 18 B'}
+      ],
+    }
+  },
+  methods:{
+    entriMahasiswa(event){
+      this.dataMahasiswa.push(event);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
